2f2c99de6349ceaa00b539c83d7b1dcf24db438f
TitusLabenis <42561133+TitusLabenis@users.noreply.github.com>
2018-08-23 13:59:33 -0700

    Added a player and an enemy, combat mechanics and movement

---
+++ Knockoff Skyrim/Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : PlayerAttack {
+
+    //To check if player is attacking
+    private const int ATTACKING = 2;
+
+    private bool enemyHit = false;
+
+    [SerializeField]
+    private float enemyHP = 10f;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PlayerSword") 
+            && enemyHit == false
+            )
+        {
+            enemyHP = enemyHP - 5f;
+            Debug.Log("Player Hit Enemy");
+            enemyHit = true;
+            StartCoroutine(CheckSwordSwingFunction());
+        }
+
+        if (enemyHP <= 0)
+        {
+            gameObject.SetActive(false);
+        } 
+
+    }
+
+    IEnumerator CheckSwordSwingFunction()
+    {
+            yield return new WaitForSeconds(0.5f);
+            enemyHit = false;
+    }
+}
---
+++ Knockoff Skyrim/Assets/_Scripts/PlayerAttack.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    //Mouse Inputs
+    private const int LEFTCLICK = 0;
+    private const int RIGHTCLICK = 1;
+    private const int MIDDLECLICK = 2;
+
+    private int swordStatus = IDLE;
+
+    //Sword Statuses
+    private const int IDLE = 0;
+    private const int ATTACK = 1;
+    private const int ATTACKING = 2;
+    private const int BLOCK = 3;
+    private const int BLOCKING = 4;
+    private const int UNBLOCK = 5;
+    private const int UNBLOCKING = 6;
+
+
+    [SerializeField]
+    private GameObject playerSword;
+
+	void Update () {
+		if (Input.GetMouseButtonDown(LEFTCLICK) && swordStatus == IDLE)
+        {
+            StartCoroutine(SwingSwordFunction());
+        }
+
+        if (Input.GetMouseButtonDown(RIGHTCLICK) && swordStatus == IDLE)
+        {
+            StartCoroutine(BlockSwordFunction());
+        }
+
+        if (Input.GetMouseButtonUp(RIGHTCLICK) && swordStatus == BLOCKING)
+        {
+            StartCoroutine(UnBlockSwordFunction());
+        }
+    }
+
+    IEnumerator UnBlockSwordFunction()
+    {
+        swordStatus = UNBLOCK;
+        playerSword.GetComponent<Animation>().Play("SwordUnblock");
+        swordStatus = UNBLOCKING;
+        yield return new WaitForSeconds(0.5f);
+        swordStatus = IDLE;
+    }
+
+    IEnumerator BlockSwordFunction()
+    {
+        swordStatus = BLOCK;
+        playerSword.GetComponent<Animation>().Play("SwordBlock");
+        swordStatus = BLOCKING;
+        yield return new WaitForSeconds(0.5f);
+    }
+
+    IEnumerator SwingSwordFunction()
+    {
+        swordStatus = ATTACK;
+        playerSword.GetComponent<Animation>().Play("SwordAttack");
+        swordStatus = ATTACKING;
+        yield return new WaitForSeconds(1.0f);
+        swordStatus = IDLE;
+    }
+}
