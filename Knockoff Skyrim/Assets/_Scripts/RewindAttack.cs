db146ffd203db82aa826f8b0abf86c98d619b61a
TitusLabenis <42561133+TitusLabenis@users.noreply.github.com>
2018-09-02 22:35:28 -0700

    Added some crappy attack blocking logic that doesn't work; REMEMBER TO FIX

---
+++ Knockoff Skyrim/Assets/_Scripts/RewindAttack.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RewindAttack : PlayerAttack {
+
+    [SerializeField]
+    private Transform startPos;
+
+    [SerializeField]
+    private Transform sword;
+
+    private bool isRewinding = false;
+
+    private const float BLOCKING = 4;
+
+    List<Vector3> positions;
+
+	// Use this for initialization
+	void Start () {
+        positions = new List<Vector3>();
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if(collision.gameObject.CompareTag("PlayerSword") && swordStatus == BLOCKING)
+        {
+            StartRewind();
+        }
+
+        if (sword == startPos)
+        {
+            StopRewind();
+        }
+    }
+
+    void StartRewind()
+    {
+        isRewinding = true;
+    }
+
+    void StopRewind()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (isRewinding == true)
+        {
+            Rewind();
+        }
+
+        else
+        {
+            Record();
+        }
+    }
+
+    private void Rewind()
+    {
+        transform.position = positions[0];
+        positions.RemoveAt(0);
+    }
+
+    private void Record()
+    {
+        positions.Insert(0, transform.position);
+    }
+}
