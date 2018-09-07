c4964ef199ee61df759625a79c25fd6a3ec205b2
TitusLabenis <42561133+TitusLabenis@users.noreply.github.com>
2018-09-06 21:31:28 -0700

    Made a death screen (Not implemented yet)

---
+++ Knockoff Skyrim/Assets/_Scripts/Player/PlayerDeath.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour {
+
+    [SerializeField]
+    private float playerHP = 1.0f;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (playerHP <= 0f)
+        {
+            //Death();
+        }
+    }
+
+    private void OnTriggerEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("EnemySword"))
+        {
+            TakeDamage();
+        }
+    }
+
+    private void TakeDamage()
+    {
+        playerHP = playerHP - 1.0f;
+    }
+
+    private void Death()
+    {
+
+    }
+}
